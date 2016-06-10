2932ddc4b52d55ebbf89e93db81929fc7aab2c55
xxldoctor <xxldoctor@gmail.com>
2016-06-10 16:36:49 +0300

    Update

--- allseatings.py
+++ allseatings.py
@@ -1,3 +1,36 @@
+def crossnumbers(list1, list2):
+	result = 0
+	for i in list1:
+		if i in list2:
+			result += 1
+	return result
+
+
+def partmaker(games):
+	collector = set()
+	part = {}
+	gamestodelete = []
+	for i in games:
+		if set(games[i]).isdisjoint(collector):
+			collector.update(set(games[i]))
+			part[i] = games[i]
+			gamestodelete.append(i)
+	gamescleaner(games, gamestodelete)
+	return part
+
+def gamescleaner(games, deletelist):
+	for i in deletelist:
+		games.pop(i)
+	return
+
+# pairs = set()
+# for i1 in range(len(players)-1):
+# 	for i2 in range(i1 + 1, len(players)):
+# 		a = tuple([i1, i2])
+# 		pairs.add(a)
+# print(pairs)
+
+
 players = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
 games = {}
 x = 0
@@ -7,33 +40,9 @@ for i1 in range(len(players)-3):
 			for i4 in range(i3 + 1, len(players)):
 				x += 1
 				games[x] = [players[i1], players[i2], players[i3], players[i4]]
-
-
-def crossnumbers(list1, list2):
-	result = 0
-	for i in list1:
-		if i in list2:
-			result += 1
-	return result
-
-part = {}
-collector = set()
-gamestodelete = []
-for i in games:
-	if set(games[i]).isdisjoint(collector):
-		collector.update(set(games[i]))
-		part[i] = games[i]
-		gamestodelete.append(i)
-
-for i in gamestodelete:
-	games.pop(i)
-gamestodelete = []
-print(collector)
-print(part)
-
-pairs = set()
-for i1 in range(len(players)-1):
-	for i2 in range(i1 + 1, len(players)):
-		a = tuple([i1, i2])
-		pairs.add(a)
-print(pairs)
+# print(games)
+gamessessions = []
+for i in range(2):
+	gamessessions.append(partmaker(games))
+	print(gamessessions[i])
+# print(games)
